df62f24a850aa535b7ac65e45bbaf08ab8b40a30
LucasMZ <55422065+LucasMZReal@users.noreply.github.com>
2021-09-24 01:19:09 -0300

    Create init.lua

---
+++ src/init.lua
@@ -0,0 +1,47 @@
+local TweenService: TweenService = game:GetService("TweenService")
+local FreeThread: thread? = nil
+
+local function DestroyTween(tween: Tween)
+	local thread = FreeThread :: thread
+	FreeThread = nil
+
+	while true do
+		tween.Completed:Wait()
+		tween:Destroy()
+
+		FreeThread = thread
+		tween = coroutine.yield() :: Tween
+	end
+end
+
+return function(
+	instance: Instance,
+	tweenInfo: TweenInfo,
+	properties: any
+): Tween
+
+	assert(
+		typeof(instance) == 'Instance',
+		"Must be an instance"
+	)
+
+	assert(
+		typeof(tweenInfo) == 'TweenInfo',
+		"Must be TweenInfo"
+	)
+
+	assert(
+		typeof(properties) == 'table',
+		"Must be a table"
+	)
+
+	local tween: Tween = TweenService:Create(instance, tweenInfo, properties)
+	tween:Play()
+
+	if FreeThread == nil then
+		FreeThread = coroutine.create(DestroyTween) :: thread
+	end
+	task.spawn(FreeThread :: thread, tween)
+
+	return tween
+end
